2ab80c80ccf7b15e17d68bb7b9dfe83c53cdc1c1
Valeran Maytie <valeran.maytie@universite-paris-saclay.fr>
2023-11-14 11:20:27 +0100

    up

---
+++ comb/course.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from travo import GitLab
+from travo.jupyter_course import JupyterCourse
+from travo.script import main
+
+forge = GitLab("https://gitlab.dsi.universite-paris-saclay.fr/")
+course = JupyterCourse(
+                 forge=forge,
+                 path="M1InfoMPRICombAlg",
+                 name="Combinatoire et Calcul Algébrique",
+                 student_dir="./",
+                 session_path="2022-2023",
+                 script="./course.py",
+                 expires_at="2023-12-31",
+                 jobs_enabled_for_students=False)
+
+usage = f"""Aide pour l'utilisation de la commande {course.script}
+===============================================
+
+Télécharger ou mettre à jour un TP (remplacer «Seance1» par le nom du TP):
+
+    cd ~/M1InfoMPRI/CombAlg
+    {course.script} fetch Seance1
+
+Déposer un TP (remplacer «Seance1» par le nom du TP):
+
+    cd ~/M1InfoMPRI/CombAlg
+    {course.script} submit Seance1
+
+
+Plus d'aide:
+
+    {course.script} --help
+"""
+
+if __name__ == "__main__":
+    main(course, usage)
